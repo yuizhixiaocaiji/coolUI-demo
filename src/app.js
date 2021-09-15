c8971e79c8b07d2a793fa293670e6c48f1feefeb
你çxuCheng <1390683299@qq.com>
2021-09-15 10:34:16 +0800

    使用 chai.expect 添加4个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -15,3 +15,85 @@ new Vue({
     loading3: false
   }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+//单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  //mock
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+}

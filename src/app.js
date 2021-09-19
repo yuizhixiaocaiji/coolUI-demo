f1ede2b546d4d97fb962ec27183bf5fba9071666
你çxuCheng <1390683299@qq.com>
2021-09-19 18:07:55 +0800

    实现 position：top/bottom/middle  功能。

--- src/app.js
+++ src/app.js
@@ -40,13 +40,29 @@ new Vue({
     message: 'hi'
   },
   created() {
-    this.$toast('很多', {
-      enableHtml: true,
-    })
   },
   methods: {
-    showToast() {
-
+    showToast1() {
+      this.showToast('top')
+    },
+    showToast2() {
+      this.showToast('middle')
+    },
+    showToast3() {
+      this.showToast('bottom')
+    },
+    showToast(position) {
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+        position: 'bottom',
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback() {
+            console.log('他说已经充值智商了')
+          }
+        },
+        autoClose: 3,
+      })
     }
   }
 })
--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template >
-  <div class="toast" ref="wrapper" >
+  <div class="toast" ref="wrapper" :class="toastClasses" >
     <div class="message" >
       <slot v-if="!enableHtml" ></slot >
       <div v-else v-html="$slots.default[0]" ></div >
@@ -20,7 +20,7 @@ export default {
     },
     autoCloseDelay: {
       type: Number,
-      default: 50
+      default: 3
     },
     closeButton: {
       type: Object,
@@ -34,11 +34,23 @@ export default {
     enableHtml: {
       type: Boolean,
       default: false
+    },
+    position: {
+      type: String,
+      default: 'top',
+      validator(value) {
+        return [ 'top', 'bottom', 'middle' ].indexOf(value) >= 0
+      }
     }
   },
   mounted() {
     this.updateStyles()
     this.execAutoClose()
+  },
+  computed: {
+    toastClasses() {
+      return { [`position-${this.position}`]: true }
+    }
   },
   methods: {
     updateStyles() {
@@ -58,9 +70,6 @@ export default {
       this.$el.remove()
       this.$destroy()
     },
-    log() {
-      console.log('测试')
-    },
     onClickClose() {
       this.close()
       if (this.closeButton && typeof this.closeButton.callback === 'function') {
@@ -79,9 +88,7 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   line-height: 1.8;
   min-height: $toast-min-height;
   position: fixed;
-  top: 0;
   left: 50%;
-  transform: translateX(-50%);
   display: flex;
   align-items: center;
   color: white;
@@ -103,6 +110,21 @@ $toast-bg: rgba(0, 0, 0, 0.75);
     height: 100%;
     border-left: 1px solid #666;
     margin-left: 16px;
+  }
+
+  &.position-top {
+    top: 0;
+    transform: translateX(-50%);
+  }
+
+  &.position-bottom {
+    bottom: 0;
+    transform: translateX(-50%);
+  }
+
+  &.position-middle {
+    top: 50%;
+    transform: translate(-50%, -50%);
   }
 }
 

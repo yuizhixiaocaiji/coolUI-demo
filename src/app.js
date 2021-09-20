d3efa4e3b3799a6300036f87a7edf4c1f69cfd8c
你çxuCheng <1390683299@qq.com>
2021-09-20 11:50:32 +0800

    添加 tabs 相关组件， 添加基本 props

--- src/app.js
+++ src/app.js
@@ -12,6 +12,11 @@ import Content from './content.vue'
 import Footer from './footer.vue'
 import Toast from './toast.vue'
 import plugin from './plugin.js'
+import Tabs from './tabs.vue'
+import TabsHead from './tabs-head.vue'
+import TabsBody from './tabs-body.vue'
+import TabsItem from './tabs-item.vue'
+import TabsPane from './tabs-pane.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -25,6 +30,11 @@ Vue.component('g-sider', Sider)
 Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-toast', Toast)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
 Vue.use(plugin)
 
 import createElement from 'vue'
@@ -34,10 +44,7 @@ const h = createElement
 new Vue({
   el: '#app',
   data: {
-    loading1: false,
-    loading2: true,
-    loading3: false,
-    message: 'hi'
+    selectedTab: 'sports'
   },
   created() {
   },

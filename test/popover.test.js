e98638fbeff139f7c7f18e8988bdd1605d44d989
你çxuCheng <1390683299@qq.com>
2021-09-23 19:44:57 +0800

    有一个测试用例不知道怎么实现，先跳过

--- test/popover.test.js
+++ test/popover.test.js
@@ -31,4 +31,27 @@ describe('Popover', (done) => {
       done()
     })
   })
+  xit('可以设置 trigger', (done)=>{
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <g-popover trigger="hover" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>点我</button>
+    </g-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    let event = new Event('mouseenter')
+    vm.$el.dispatchEvent(event)
+    vm.$nextTick(() => {
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper).to.exist
+      done()
+    })
+  });
 })

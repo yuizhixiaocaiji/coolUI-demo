6358b05d84371e52d1021948e632d75b887089e2
你çxuCheng <1390683299@qq.com>
2021-09-25 14:48:12 +0800

    完成 collapse 组件文档书写

---
+++ docs/.vuepress/components/collapse-demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <g-collapse :selected.sync="selected">
+      <g-collapse-item title="标题1" name="1">1</g-collapse-item>
+      <g-collapse-item title="标题2" name="2">2</g-collapse-item>
+      <g-collapse-item title="标题3" name="3">3</g-collapse-item>
+    </g-collapse>
+  </div>
+</template>
+
+<script>
+import Collapse from '../../../src/collapse'
+import CollapseItem from '../../../src/collapse-item'
+export default {
+  components:{
+    'g-collapse': Collapse,
+    'g-collapse-item': CollapseItem,
+  },
+  data(){
+    return{
+      selected : ['1']
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

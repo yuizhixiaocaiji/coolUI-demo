da37410a54648c7afc1d46b5994039056ccec20c
你çxuCheng <1390683299@qq.com>
2021-09-25 09:07:26 +0800

    完成 button 组件的编写

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="button-wrapper">
+    <g-button>默认按钮</g-button>
+    <g-button icon="settings">默认按钮</g-button>
+    <g-button icon="settings" icon-position="right">默认按钮</g-button>
+    <g-button :loading="true">默认按钮</g-button>
+    <g-button disabled>默认按钮</g-button>
+  </div>
+</template>
+
+<script>
+import Button from '../../../src/Button'
+export default {
+  components: {
+    'g-button': Button,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button-wrapper{
+  padding: 0 10px;
+}
+</style>
--- src/button.vue
+++ src/button.vue
@@ -32,6 +32,14 @@ export default {
 }
 </script >
 <style lang="scss" scoped >
+$font-size: 14px;
+$button-height: 32px;
+$button-bg: white;
+$button-active-bg: #eee;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
 @keyframes spin {
   0% {
     transform: rotate(0deg);
@@ -42,23 +50,23 @@ export default {
 }
 
 .g-button {
-  font-size: var(--font-size);
-  height: var(--button-height);
+  font-size: $font-size;
+  height: $button-height;
   padding: 0 1em;
-  border-radius: var(--border-radius);
-  border: 1px solid var(--border-color);
-  background: var(--button-bg);
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   vertical-align: middle;
 
   &:hover {
-    border-color: var(--border-color-hover);
+    border-color: $border-color-hover;
   }
 
   &:active {
-    background-color: var(--button-active-bg);
+    background-color: $button-active-bg;
   }
 
   &:focus {

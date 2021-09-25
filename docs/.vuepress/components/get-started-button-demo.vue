9e1f30ec13bfe53df7b00472b29e19f72b1f93f0
你çxuCheng <1390683299@qq.com>
2021-09-25 11:09:57 +0800

    完成官网雏形以及部署

---
+++ docs/.vuepress/components/get-started-button-demo.vue
@@ -0,0 +1,17 @@
+<template >
+  <div >
+    <br >
+    <g-button >默认按钮</g-button >
+  </div >
+</template >
+
+<script >
+import Button from '../../../src/button'
+
+export default {
+  name: 'Init-get-started-button-demo',
+  components: {
+    'g-button': Button,
+  },
+}
+</script >

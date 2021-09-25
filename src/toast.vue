442d2768724345c3ff48515f5ae365ce2c66d0cc
你çxuCheng <1390683299@qq.com>
2021-09-25 14:39:17 +0800

    完成 toast 组件文档书写

--- src/toast.vue
+++ src/toast.vue
@@ -1,34 +1,37 @@
 <template >
-  <div class="wrapper" :class="toastClasses" >
-    <div class="toast" ref="toast" >
-      <div class="message" >
+  <div class="toast-wrapper-center" :class="toastClasses" ref="toastWrapper" >
+    <div class="toast-wrapper" ref="wrapper" >
+      <div class="toast-message" >
         <slot v-if="!enableHtml" ></slot >
         <div v-else v-html="$slots.default[0]" ></div >
       </div >
-      <div class="line" ref="line" ></div >
-      <span v-if="closeButton" class="close" @click="onClickClose" >
+      <div class="toast-line" ref="line" ></div >
+      <span class="toast-close" @click="onClickClose" >
       {{ closeButton.text }}
     </span >
     </div >
   </div >
 </template >
-<script>
-//构造组件的选项
+
+<script >
 export default {
-  name: 'GuluToast',
+  name: 'InitToast',
   props: {
     autoClose: {
-      type: [Boolean, Number],
+      type: [ Boolean, Number ],
       default: 5,
-      validator (value) {
-        return value === false || typeof value === 'number';
+      validator(value) {
+        if (value === false || typeof value === 'number') {
+          return true
+        }
       }
     },
     closeButton: {
       type: Object,
-      default () {
+      default() {
         return {
-          text: '关闭', callback: undefined
+          text: '关闭',
+          callback: undefined
         }
       }
     },
@@ -39,65 +42,70 @@ export default {
     position: {
       type: String,
       default: 'top',
-      validator (value) {
-        return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+      validator(value) {
+        return [ 'top', 'middle', 'bottom' ].indexOf(value) >= 0
+      }
+    },
+    zIndex: {
+      type: Number,
+      default: 20,
+      validator(value) {
+        return (typeof value) === 'number'
       }
     }
   },
-  mounted () {
+  mounted() {
+    this.$refs.toastWrapper.style.zIndex = this.zIndex
     this.updateStyles()
     this.execAutoClose()
   },
   computed: {
-    toastClasses () {
+    toastClasses() {
       return {
         [`position-${this.position}`]: true
       }
-    }
+    },
   },
   methods: {
-    updateStyles () {
+    close() {
+      this.$el.remove()
+      this.$emit('beforeClose')
+      this.$destroy()
+    },
+    updateStyles() {
       this.$nextTick(() => {
         this.$refs.line.style.height =
-            `${this.$refs.toast.getBoundingClientRect().height}px`
+            `${this.$refs.wrapper.getBoundingClientRect().height}px`
       })
     },
-    execAutoClose () {
+    execAutoClose() {
       if (this.autoClose) {
         setTimeout(() => {
           this.close()
         }, this.autoClose * 1000)
       }
     },
-    close () {
-      this.$el.remove()
-      this.$emit('close')
-      this.$destroy()
+    log() {
+      console.log('测试 Toast 中 onClickClose 事件的 callback,如果要使用就传入 toast，然后toast.log()即可')
     },
-    onClickClose () {
+    onClickClose() {
       this.close()
       if (this.closeButton && typeof this.closeButton.callback === 'function') {
-        this.closeButton.callback(this)//this === toast实例
+        this.closeButton.callback(this)
       }
     }
   }
 }
-</script>
+</script >
+
 <style lang="scss" scoped >
 $font-size: 14px;
 $toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.75);
-@keyframes slide-up {
-  0% {
-    opacity: 0;
-    transform: translateY(100%);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0%)
-  }
-}
-
+$toast-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.50);
+$toast-radius: 4px;
+$space-between: 16px;
+$animation-duration: 300ms;
 @keyframes slide-down {
   0% {
     opacity: 0;
@@ -105,7 +113,7 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   }
   100% {
     opacity: 1;
-    transform: translateY(0)
+    transform: translateY(0%);
   }
 }
 
@@ -118,69 +126,75 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   }
 }
 
-.wrapper {
+@keyframes slide-up {
+  0% {
+    opacity: 0;
+    transform: translateY(100%)
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%)
+  }
+}
+
+.toast-wrapper-center {
+  z-index: 20;
   position: fixed;
   left: 50%;
   transform: translateX(-50%);
-  $animation-duration: 300ms;
 
   &.position-top {
     top: 0;
 
-    .toast {
-      border-top-right-radius: 0;
+    .toast-wrapper {
       border-top-left-radius: 0;
+      border-top-right-radius: 0;
       animation: slide-down $animation-duration;
-    }
-  }
-
-  &.position-bottom {
-    bottom: 0;
-
-    .toast {
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-      animation: slide-up $animation-duration;
     }
   }
 
   &.position-middle {
     top: 50%;
     transform: translateX(-50%) translateY(-50%);
+    animation: fade-in $animation-duration;
+  }
 
-    .toast {
-      animation: fade-in $animation-duration;
+  &.position-bottom {
+    bottom: 0;
+
+    .toast-wrapper {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      animation: slide-up $animation-duration;
+    }
+  }
+
+  .toast-wrapper {
+    font-size: $font-size;
+    min-height: $toast-min-height;
+    line-height: 1.8em;
+    background: $toast-bg;
+    box-shadow: $toast-box-shadow;
+    padding: 0 $space-between;
+    border-radius: $toast-radius;
+    color: white;
+    display: flex;
+    align-items: center;
+
+    .toast-message {
+      padding: $space-between/2 0;
+    }
+
+    .toast-line {
+      height: 100%;
+      border-left: 1px solid #666666;
+      margin-left: $space-between;
+    }
+
+    .toast-close {
+      padding-left: $space-between;
+      flex-shrink: 0;
     }
   }
 }
-
-.toast {
-  font-size: $font-size;
-  line-height: 1.8;
-  min-height: $toast-min-height;
-  display: flex;
-  align-items: center;
-  color: white;
-  background: $toast-bg;
-  border-radius: 4px;
-  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.50);
-  padding: 0 16px;
-
-  .message {
-    padding: 8px 0;
-  }
-
-  .close {
-    padding-left: 16px;
-    flex-shrink: 0;
-  }
-
-  .line {
-    height: 100%;
-    border-left: 1px solid #666;
-    margin-left: 16px;
-  }
-}
-
-
 </style >
